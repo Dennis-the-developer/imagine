d769dbd7a692bb41b1bb6816236af3fab2e12aac
Dennis Appah <dev.dennisappah@gmail.com>
2024-06-11 11:12:22 +0000

    Array methods in javascript

--- public/js/script.js
+++ public/js/script.js
@@ -159,3 +159,43 @@ return 'Participants added!';
 const alphabets = ['A', 'B', 'C', 'D'];
 alphabets.join(' ');
 alphabets.join(' ').split(' ')
+
+//Write a function that will take a user with firstname, lastname and return fullname;
+const user = {
+  firstname: 'Joe',
+  lastname: 'Garner',
+}
+function fullname(user){
+  return {
+    ...user,
+    fullname: `${user.firstname} ${user.lastname}`
+  }
+}
+
+fullname(user);
+
+//Array map
+const users = [
+  {firstname: 'Linda', lastname: 'Parker'},
+  {firstname: 'Mark', lastname: 'Twaine'},
+  {firstname: 'Oliver', lastname: 'Queen'},
+  {firstname: 'Stephen', lastname: 'Grider'},
+  {firstname: 'Elon', lastname: 'Musk'}
+]
+users.map(fullname);
+
+//Map and return squares of numbers
+function square(number){
+  return number**2;
+}
+
+const numbers = [0,1,2,3,4,5]
+numbers.map(square);
+
+
+//Filter and return even numbers
+function even(number){
+  return number % 2 === 0;
+}
+
+numbers.filter(even);
